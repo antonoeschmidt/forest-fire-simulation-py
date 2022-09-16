78a0b948bd035676fe8a933e4491e50fbf156e21
asc-it-minds <asc@it-minds.dk>
2022-09-16 10:38:59 +0200

    Initial commit

---
+++ test_sim.py
@@ -0,0 +1,10 @@
+import simpy
+
+# Set up the environment
+env = simpy.Environment()
+
+# Assume you've defined checkpoint_run() beforehand
+env.process(checkpoint_run(env, num_booths, check_time, passenger_arrival))
+
+# Let's go!
+env.run(until=10)
