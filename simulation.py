73a3fc8a1934d8cc63f58f344ba00b006679a9a2
Thyge Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2022-10-14 11:17:20 +0200

    add setup

--- simulation.py
+++ simulation.py
@@ -6,6 +6,24 @@ def fire(env, forest):
     yield env.timeout(1)
     forest.step()
 
+
+# Modify this to work as in https://simpy.readthedocs.io/en/latest/examples/carwash.html 
+def setup(env, num_machines, washtime, t_inter):
+    """Create a carwash, a number of initial cars and keep creating cars
+    approx. every ``t_inter`` minutes."""
+    # Create the carwash
+    carwash = Carwash(env, num_machines, washtime)
+
+    # Create 4 initial cars
+    for i in range(4):
+        env.process(car(env, 'Car %d' % i, carwash))
+
+    # Create more cars while the simulation is running
+    while True:
+        yield env.timeout(random.randint(t_inter - 2, t_inter + 2))
+        i += 1
+        env.process(car(env, 'Car %d' % i, carwash))
+
 if __name__ == "__main__":
     env = simpy.Environment()
 
