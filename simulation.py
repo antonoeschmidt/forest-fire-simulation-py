a67655a210def0bea74e44dac646cd57b4584527
Thyge Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2022-10-17 19:24:39 +0200

    Working version using asyncio with two co-routines

--- simulation.py
+++ simulation.py
@@ -1,4 +1,8 @@
 import asyncio
+import json
+import time
+from threading import Thread
+import queue
 
 import simpy
 import websockets
@@ -7,28 +11,44 @@ from ca.cellular_automaton import CellularAutomaton
 from ca.simple_fire import SimpleCa
 
 
-def fire_progression(env: simpy.core.Environment, forest: CellularAutomaton, websocket):
+def fire_progression(env: simpy.core.Environment, forest: CellularAutomaton):
     while True:
         yield env.timeout(1)
         forest.step()
         data = {'grid': forest.data(), 'grid_size': 20}
-        await websocket.send(data)
+        queue.put(data)
 
 
-async def program(websocket):
+async def program():
     forest = SimpleCa(20, 20)
-
+    forest.ignite(2, 2)
     env = simpy.Environment()
 
-    env.process(fire_progression(env, forest, websocket))
+    env.process(fire_progression(env, forest))
 
     env.run(until=100)
 
 
+async def handler(websocket):
+    while True:
+        item = queue.get()
+        await websocket.send(json.dumps(item))
+        time.sleep(1)
+
+
 async def main():
-    async with websockets.serve(program, "localhost", 8000):
+    async with websockets.serve(handler, "localhost", 8000):
         await asyncio.Future()  # run forever
 
 
+queue = queue.Queue()
+
 if __name__ == "__main__":
+    asyncio.run(program())
     asyncio.run(main())
+
+    # simulation = Thread(target=program, args=(queue,))
+    # websocket1 = Thread(target=main, args=(queue,))
+    #
+    # simulation.start()
+    # websocket1.start()
