fc3cb245225e59d0ec4d760e5887329c36548852
Thyge Steffensen <31892312+thygesteffensen@users.noreply.github.com>
2022-11-11 14:07:07 +0100

    Fix lint

--- ca/simple_cell.py
+++ ca/simple_cell.py
@@ -43,7 +43,6 @@ class SimpleCa(CellularAutomaton):
         if original.burned:
             return new
 
-
         # Determine how long a single cell should burned depending on vegation
         if VegetationType.LOW_VEG.value == original.veg and original.fire > 10:
             return new.factory(burned=True, fire_intensity=0)
@@ -56,12 +55,11 @@ class SimpleCa(CellularAutomaton):
 
         # Should I burn?
         (x_wind, y_wind) = new.wind
-        if(x== 1, y==1):
-            print(f'wind: {x_wind} and {y_wind}')
-        wind_strength = math.sqrt(x_wind**2 + y_wind**2)
-        wind_strength = wind_strength*3
+
+        wind_strength = math.sqrt(x_wind ** 2 + y_wind ** 2)
+        wind_strength = wind_strength * 3
         if original.fire > 0:
-            # Fire Intensity ==============================================================================================
+            # Fire Intensity ===========================================================================================
             # 🪵 More wood == more fire 🔥
             burn_factor = 0
             if VegetationType.LOW_VEG.value == original.veg:
@@ -73,8 +71,6 @@ class SimpleCa(CellularAutomaton):
             else:
                 pass
 
-            
-
             new = new.factory(fire_intensity=wind_strength + burn_factor)
             return new  # I'm already burning
 
@@ -82,13 +78,13 @@ class SimpleCa(CellularAutomaton):
 
         # gets the list of cells the vector goes through
         # https://en.wikipedia.org/wiki/Bresenham's_line_algorithm
-        cells = list(bresenham(x, y, x-x_wind, y-y_wind))
+        cells = list(bresenham(x, y, x - x_wind, y - y_wind))
 
         # check normal boundaries
         for (x_cell, y_cell) in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
             if self.get(x_cell, y_cell) is not None:
                 summed_intensity = summed_intensity + \
-                    self.get(x_cell, y_cell).fire_intensity
+                                   self.get(x_cell, y_cell).fire_intensity
 
         # check cells in the wind direction
         for cell in cells:
@@ -96,7 +92,7 @@ class SimpleCa(CellularAutomaton):
             if self.get(x_cell, y_cell) is not None:
                 summed_intensity = summed_intensity + self.get(x_cell, y_cell).fire_intensity
 
-        lucky_number = random.randrange(0,100)
+        lucky_number = random.randrange(0, 100)
         if summed_intensity > lucky_number:
             new = new.factory(fire=1)
 
